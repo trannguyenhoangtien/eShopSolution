c625a25343d054102241d6aecda5310358d558c3
Tien Hoàng <hoangtien@huongnghiepaau.com>
2020-10-01 17:08:12 +0700

    Add Seed data

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -12,8 +12,6 @@ namespace eShopSolution.Data.Entities
         public int Stock { set; get; }
         public int ViewCount { set; get; }
         public DateTime DateCreated { set; get; }
-        public string SeoAlias { set; get; }
-
         public List<ProductInCategory> ProductInCategories { get; set; }
 
         public List<OrderDetail> OrderDetails { get; set; }
