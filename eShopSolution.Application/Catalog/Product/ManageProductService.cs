976c379bd055893fdca915acdb7d82186b4ac10e
hoangtien2510 <hoangtien103@gmail.com>
2020-10-01 21:57:14 +0700

    Add Application Layer

---
+++ eShopSolution.Application/Catalog/Product/ManageProductService.cs
@@ -0,0 +1,44 @@
+﻿using eShopSolution.Application.Catalog.Product.Dtos;
+using eShopSolution.Application.Dtos;
+using eShopSolution.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Product
+{
+    public class ManageProductService : IManageProductService
+    {
+        private readonly EShopDbContext _context;
+        public ManageProductService(EShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> Create(ProductCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(ProductEditRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eShopSolution.Application/Catalog/Product/PublicProductService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Product
+{
+    public class PublicProductService
+    {
+    }
+}
