7e161340807358c3bb108ddfc3957325776f3668
hoangtien2510 <hoangtien103@gmail.com>
2020-10-04 17:49:35 +0700

    create api Login and Register

---
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authenticate(LoginRequest request);
+
+        Task<IEnumerable<IdentityError>> Register(RegisterRequest request);
+    }
+}
